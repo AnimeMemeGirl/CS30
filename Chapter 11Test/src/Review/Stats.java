9222928fffaf4cb8b3fa938d31652348b5dec0fb
74013002 <74013002@L815WD220032.edu.cbe.ab.ca>
2022-04-11 11:48:23 -0600

    Yuri good

--- Chapter 11Test/src/Review/Stats.java
+++ Chapter 11Test/src/Review/Stats.java
@@ -8,21 +8,39 @@ public class Stats
 	public static void main(String[] args)
 	{
 		
-		File textFile = new File("newF.txt");
+		File dataFile = new File("C:\\Users\\74013002\\git\\CS30\\Chapter 11Test\\src\\Review\\newF.txt");
 		FileReader in;
 		BufferedReader readFile;
-		String lineOfText;
+		String StuName, Score;
+		double scoreValue = 0;
+		double lowScore = 100;
+		double highScore = 0;
+		double AvgScore;
+		double totalScore = 0;
+		int numScores = 0;
 		
 		try
 		{
 			
-			in = new FileReader(textFile);
+			in = new FileReader(dataFile);
 			readFile = new BufferedReader(in);
 			
-			while((lineOfText = readFile.readLine()) != null)
+			while((StuName = readFile.readLine()) != null)
 			{
-				System.out.println(lineOfText);
-			}
+				Score = readFile.readLine();
+				System.out.println(StuName + " " + Score);
+				numScores += 1;
+				
+				if(scoreValue < lowScore)
+				{
+					lowScore = scoreValue;
+				}
+				else if(scoreValue > highScore)
+				{
+					highScore = scoreValue;
+				}
+				
+			}//while loop
 			readFile.close();
 			in.close();
 		}catch(FileNotFoundException e)
