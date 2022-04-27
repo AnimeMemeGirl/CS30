99d16db2dda5b7bc4d8df97932be7388a69a91c5
74013002 <74013002@L815WD220032.edu.cbe.ab.ca>
2022-04-27 11:42:03 -0600

    Yuri Good

--- Chapter9/src/Chapter9/FindName.java
+++ Chapter9/src/Chapter9/FindName.java
@@ -1,8 +1,26 @@
 package Chapter9;
 
+import java.util.Scanner;
+
 public class FindName 
 {
 
+	public static void main(String[] args)
+	{
+		
+	   Scanner input = new Scanner(System.in);
+	   String name;
+	   String[] nameCount = new String[5];
 	
+	
+       for(int i = 0; i < nameCount.length;i ++)
+       {
+    	   
+    	   System.out.println("Please Enter A Name: ");
+    	   name = input.next();
+    	   nameCount[i] = name;
+       }
+    
+	}
 	
 }
---
+++ Chapter9/src/Chapter9/Search.java
@@ -0,0 +1,27 @@
+package Chapter9;
+
+public class Search 
+{
+
+	public static int linear(int[] array, int numToFind)
+	{
+		
+		int index = 0;
+		
+		while ((array[index] != numToFind) && (index < array.length -1));
+		{
+			index += 1;
+		}
+		
+		if(array[index] == numToFind)
+		{
+			return(index);
+		}
+		else
+		{
+			return(-1);
+		}
+		
+	}
+	
+}
