9f4599cd2fab1dec4c2b5b06557b2cc43082ed39
74013002 <74013002@L815WD220032.edu.cbe.ab.ca>
2022-04-28 10:10:26 -0600

    Yuri Good

--- Chapter9/src/Chapter9/FindName.java
+++ Chapter9/src/Chapter9/FindName.java
@@ -9,16 +9,34 @@ public class FindName
 	{
 		
 	   Scanner input = new Scanner(System.in);
-	   String name;
+	   String name = null; 
+	   int location;
 	   String[] nameCount = new String[5];
 	
 	
+	   //asks the users for names
        for(int i = 0; i < nameCount.length;i ++)
        {
     	   
     	   System.out.println("Please Enter A Name: ");
+    	 
+    	   nameCount[i] = input.next();
+       }
+       
+	   //asks the users for a name to search for
+    	   System.out.println("Enter a name that you would like to search for: ");
     	   name = input.next();
-    	   nameCount[i] = name;
+       
+       
+       //search for a name
+       location = Search.linear(nameCount, name);
+       if (location == -1)
+       {
+    	   System.out.println("Sorry, name has not been found in is array.");
+       }
+       else
+       {
+    	   System.out.println("First occurence of this name: " + location);
        }
     
 	}
--- Chapter9/src/Chapter9/Search.java
+++ Chapter9/src/Chapter9/Search.java
@@ -3,17 +3,38 @@ package Chapter9;
 public class Search 
 {
 
-	public static int linear(int[] array, int numToFind)
+	public static int linear(int[] nameCount, int name)
 	{
 		
 		int index = 0;
 		
-		while ((array[index] != numToFind) && (index < array.length -1));
+		while ((nameCount[index] != name) && (index <  nameCount.length -1));
 		{
 			index += 1;
 		}
 		
-		if(array[index] == numToFind)
+		if(nameCount[index] == name)
+		{
+			return(index);
+		}
+		else
+		{
+			return(-1);
+		}
+		
+	}
+	
+	public static int linear(String[] nameCount, String name)
+	{
+		
+		int index = 0;
+		
+		while (!(nameCount[index] .equals(name)) && (index <  nameCount.length -1))
+		{
+			index += 1;
+		}
+		
+		if(nameCount[index].equals(name))
 		{
 			return(index);
 		}
