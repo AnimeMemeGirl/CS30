57236d553fce9705346f793ced1401876bdd87e6
74013002 <74013002@L815WD220032.edu.cbe.ab.ca>
2022-05-03 10:09:57 -0600

    Yuri Good

---
+++ GettingStartedv2/src/gettingStarted/BlinkLEDPr1.java
@@ -0,0 +1,49 @@
+/*
+
+Program: BlinkLEDPr1.java
+
+Purpose:
+
+Author: Luke Van Hardeveld 
+School: CHHS
+Course: Computer Science 30
+ 
+
+*/
+
+package gettingStarted;
+
+//Add Phidgets Library 
+import com.phidget22.*;
+
+public class BlinkLEDPr1 
+{
+    //Handle Exceptions 
+    public static void main(String[] args) throws Exception
+    {
+
+        //Create 
+    	 DigitalOutput redLED = new DigitalOutput();
+
+        //Address 
+    	 redLED.setHubPort(1);
+	     redLED.setIsHubPortDevice(true);
+
+        //Open 
+        redLED.open(1000);
+
+        //Use your Phidgets 
+        while(true)
+        {
+            redLED.setState(true);
+            Thread.sleep(1000);
+            redLED.setState(false);
+            Thread.sleep(1000);
+        }
+    }
+}
+/* Screen Dump
+
+
+
+ */
---
+++ GettingStartedv2/src/gettingStarted/BlinkLEDPr3.java
@@ -0,0 +1,47 @@
+/*
+
+Program: BlinkLEDPr3.java
+
+Purpose:
+
+Author: Luke Van Hardeveld 
+School: CHHS
+Course: Computer Science 30
+ 
+
+*/
+
+package gettingStarted;
+
+//Add Phidgets Library 
+import com.phidget22.*;
+
+public class BlinkLEDPr3 
+{
+	
+    //Handle Exceptions 
+ public static void main(String[] args) throws Exception{
+
+     //Create 
+     DigitalOutput greenLED = new DigitalOutput();
+
+     //Address 
+     greenLED.setHubPort(4);
+     greenLED.setIsHubPortDevice(true);
+
+     //Open 
+     greenLED.open(1000);
+
+     //Use your Phidgets 
+     greenLED.setState(true);
+     Thread.sleep(1000);
+     greenLED.setState(false);
+
+ }
+
+}
+/* Screen Dump
+
+
+
+ */
