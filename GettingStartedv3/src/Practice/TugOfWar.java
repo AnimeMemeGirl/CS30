a6aa5b5815311d206e34b8dceacfad71fb5cd0be
74013002 <74013002@L815WD220032.edu.cbe.ab.ca>
2022-05-06 10:31:03 -0600

    Yuri Good

--- GettingStartedv3/src/Practice/TugOfWar.java
+++ GettingStartedv3/src/Practice/TugOfWar.java
@@ -1,5 +1,34 @@
 package Practice;
 
-public class TugOfWar {
+import com.phidget22.DigitalInput;
 
+public class TugOfWar 
+{
+
+    //Handle Exceptions 
+    public static void main(String[] args) throws Exception
+    {
+        
+        //Create 
+        DigitalInput redButton = new DigitalInput();
+        DigitalInput greenButton = new DigitalInput();
+
+        //Address 
+        redButton.setHubPort(0);
+        redButton.setIsHubPortDevice(true);
+        greenButton.setHubPort(5);
+        greenButton.setIsHubPortDevice(true);
+
+        //Open 
+        redButton.open(1000);
+        greenButton.open(1000);
+
+        //Use your Phidgets 
+        while(true)
+        {
+            System.out.println("Button State: " + redButton.getState());
+            Thread.sleep(150);
+        }
+    }
+	
 }
