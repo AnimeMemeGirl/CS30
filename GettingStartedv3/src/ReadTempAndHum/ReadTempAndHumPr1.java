05dccf17fea35758ff63461c080155ad4b28ff06
74013002 <74013002@L815WD220032.edu.cbe.ab.ca>
2022-05-10 08:59:13 -0600

    Yuri Good

---
+++ GettingStartedv3/src/ReadTempAndHum/ReadTempAndHumPr1.java
@@ -0,0 +1,30 @@
+package ReadTempAndHum;
+
+	//Add Phidgets Library 
+	import com.phidget22.*;
+	
+public class ReadTempAndHumPr1 
+{
+
+	public class GettingStarted 
+	{
+	    public void main(String[] args) throws Exception
+	    {
+
+	        //Create 
+	        HumiditySensor humiditySensor = new HumiditySensor();
+	        TemperatureSensor temperatureSensor = new TemperatureSensor();
+
+	        //Open 
+	        humiditySensor.open(1000);
+	        temperatureSensor.open(1000);
+
+	        //Use your Phidgets 
+	        while(true){
+	            System.out.println("Humidity: " + humiditySensor.getHumidity() +" %RH, Temperature: " + temperatureSensor.getTemperature() + " °C" );
+	            Thread.sleep(150);
+	        }
+	    }
+	}
+	
+}
