fc0c1aeddd0fa58c969224e15e932f07e52e51d1
74013002 <74013002@L815WD220032.edu.cbe.ab.ca>
2022-05-17 10:11:24 -0600

    Yuri Good

--- GettingStartedv3/src/Practice/TugOfWar.java
+++ GettingStartedv3/src/Practice/TugOfWar.java
@@ -1,6 +1,9 @@
 package Practice;
 
+import java.util.Scanner;
+
 import com.phidget22.DigitalInput;
+import com.phidget22.DigitalOutput;
 
 public class TugOfWar 
 {
@@ -11,34 +14,79 @@ public class TugOfWar
         
         //Create 
         DigitalInput redButton = new DigitalInput();
+        DigitalOutput redLED = new DigitalOutput();
         DigitalInput greenButton = new DigitalInput();
+        DigitalOutput greenLED = new DigitalOutput();
+        Scanner input = new Scanner(System.in);
 
         //Address 
         redButton.setHubPort(0);
         redButton.setIsHubPortDevice(true);
+        redLED.setHubPort(1);
+        redLED.setIsHubPortDevice(true);
         greenButton.setHubPort(5);
         greenButton.setIsHubPortDevice(true);
+        greenLED.setHubPort(4);
+        greenLED.setIsHubPortDevice(true);
         int gPress = 0;
         int rPress = 0;
+        boolean rState = false;
+        boolean gState = false;
 
         //Open 
         redButton.open(1000);
+        redLED.open(1000);
         greenButton.open(1000);
+        greenLED.open(1000);
 
         //Use your Phidgets 
-        while(true)
+        while(rPress < 10 && gPress < 10)
         {
         	
-        	if( redButton.getState())
+        	if(!rState && redButton.getState())
             {
+        		redLED.setState(true);
                 rPress++;
+               // System.out.println("Number of presses of red button is: " + rPress);
+                
+                
             }
-        	else if( greenButton.getState())
+        	else if(rState && !redButton.getState())
             {
-                gPress++;
+                redLED.setState(false);
             }
         	
-        }
+        	if(!gState && greenButton.getState())
+            {
+        		greenLED.setState(true);
+                gPress++;
+            }
+        	else if(gState && !greenButton.getState())
+        	{
+        		greenLED.setState(false);
+        	}
+        	
+       } 	
+        	//Flash lights
+        	redLED.setState(false);
+        	Thread.sleep(500);
+        	redLED.setState(true);
+        	Thread.sleep(500);
+        	redLED.setState(false);
+        	Thread.sleep(500);
+        	
+        	greenLED.setState(false);
+        	Thread.sleep(500);
+        	greenLED.setState(true);
+        	Thread.sleep(500);
+        	greenLED.setState(false);
+        	Thread.sleep(500);
+        	
+        	
+        	
+        	
+        	
+        
     }
 	
 }
